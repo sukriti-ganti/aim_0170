ec5a2bcde66564d2e730895e0b35084ae25af536
sukriti-ganti <sukriti.ganti.12@gmail.com>
2025-07-11 23:15:07 +0530

    Update warehouse.launch.py

--- launch/warehouse.launch.py
+++ launch/warehouse.launch.py
@@ -5,9 +5,12 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='b3rb_ros_aim_india',
-            executable='b3rb_ros_warehouse.py',
+            executable='explore',  # Use entry point from setup.py
             name='warehouse_node',
-            output='screen'
+            output='screen',
+            parameters=[
+                {'shelf_count': 5},          # Optional: adjust as needed
+                {'initial_angle': 0.0}       # Optional
+            ]
         )
     ])
-
